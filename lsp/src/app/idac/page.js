fb73af4939b596935a5702fda3b8ed19ded7d1b9
glaubervila <glauber.vila.verde@gmail.com>
2025-03-19 15:15:11 -0300

    New instalattion of React, Nextjs

---
+++ lsp/src/app/idac/page.js
@@ -0,0 +1,12 @@
+
+export default function IdacHome() {
+  return (
+    <div>
+      <main>
+        <div>
+          IDAC Home Page
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ lsp/src/app/lsp/page.js
@@ -0,0 +1,12 @@
+
+export default function LspHome() {
+  return (
+    <div>
+      <main>
+        <div>
+          LSP Home Page
+        </div>
+      </main>
+    </div>
+  );
+}
