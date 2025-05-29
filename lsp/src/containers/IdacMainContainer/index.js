418f48dde8f9863cad2ac8b8b0880f90f5eb85c6
glaubervila <glauber.vila.verde@gmail.com>
2025-05-29 14:11:08 -0300

    improve main container

--- lsp/src/containers/IdacMainContainer/index.js
+++ lsp/src/containers/IdacMainContainer/index.js
@@ -1,35 +1,17 @@
 
 "use client";
 import React from "react";
-import CssBaseline from '@mui/material/CssBaseline';
 import IdacAppBar from "@/components/IdacAppBar";
 import DevAlert from "@/components/DevAlert";
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useAuth } from "@/contexts/AuthContext";
+import MainContainer from "@/containers/MainContainer";
 
 export default function IdacMainContainer({ children }) {
-  const { settings } = useAuth();
-
-  if (Object.keys(settings).length === 0) {
-    return (
-      <div>
-        <Backdrop
-          sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
-          open={true}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      </div>
-    )
-  }
 
   return (
-    <React.Fragment>
-      <CssBaseline />
+    <MainContainer>
       <IdacAppBar />
       <DevAlert />
       {children}
-    </React.Fragment>
+    </MainContainer>
   );
 }
