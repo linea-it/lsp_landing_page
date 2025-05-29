f11bbaca40393bede594d507d40424e51351da05
glaubervila <glauber.vila.verde@gmail.com>
2025-05-29 16:12:28 -0300

    Closed #94 - Added template for deploy builded images and settings for -dev and prod environments

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -101,7 +101,7 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = EMAIL_SUBJECT_PREFIX
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
-ADMIN_URL = env("DJANGO_ADMIN_URL")
+ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin/")
 
 # Anymail
 # ------------------------------------------------------------------------------
